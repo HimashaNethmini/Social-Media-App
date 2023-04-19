185b6b69c5af4de122810bd8604c67d69eb1ef4e
HimashaNethmini <himashanethmini05@gmail.com>
2023-04-19 23:38:02 +0530

    Created Login page

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,31 @@
+import "./login.scss"
+
+const login = () => {
+  return (
+    <div className="login">
+        <div className="card">
+            <div className="left">
+                <h1>Hello World</h1>
+                <p>
+                    Hi.. This is Himasha. I am trying to do the website better.
+                    Everytime I try new thing it helps me improve a better verion of myself
+                </p>
+                <span>Don't have an account ? </span>
+                <button>Register</button>
+            </div>
+            <div className="right">
+                <h1>Login</h1>
+                <form>
+                    <input type="text" placeholder="Username" />
+                    <input type="password" placeholder="Password" />
+                    <button>Log in</button>
+
+                </form>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default login

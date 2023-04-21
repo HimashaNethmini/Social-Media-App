13cd6fcb98750ba06d33c3cef01e881dee71f46e
HimashaNethmini <himashanethmini05@gmail.com>
2023-04-21 18:31:47 +0530

    Updated files

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,12 @@
 import "./home.scss"
+import Stories from "../../component/stories/Stories";
+import Posts from "../../component/posts/Posts";
 
 const home = () => {
   return (
-    <div>
+    <div className ="home">
+      <Stories/>
+      <Posts/>
       
     </div>
   )

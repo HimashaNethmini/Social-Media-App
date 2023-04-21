27d7a63d9e12a5c475fd255a4cae54423415f9b5
HimashaNethmini <himashanethmini05@gmail.com>
2023-04-21 10:08:05 +0530

    Make Right side of the homepage

---
+++ src/component/rightbar/Rightbar.jsx
@@ -0,0 +1,161 @@
+import "./rightbar.scss"
+
+
+const rightbar = () => {
+  return (
+    <div className="rightbar">
+      <div className="container">
+        <div className="item">
+          <span>SUggestions For You</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://www.pexels.com/photo/woman-standing-on-a-rock-under-a-waterfall-15197689/" alt="" />
+              <span>Himasha Nethmini</span>
+          </div>
+          <div className="buttons">
+            <button>Follow</button>
+            <button>Dismiss</button>
+          </div>
+        </div>
+
+        <div className="user">
+            <div className="userInfo">
+              <img src="https://www.pexels.com/photo/woman-standing-on-a-rock-under-a-waterfall-15197689/" alt="" />
+              <span>Himasha Nethmini</span>
+          </div>
+          <div className="buttons">
+            <button>Follow</button>
+            <button>Dismiss</button>
+          </div>
+        </div>
+        </div>
+        <div className="item">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://www.pexels.com/photo/woman-standing-on-a-rock-under-a-waterfall-15197689/" alt="" />
+              <p>
+                <span>Himasha Nethmini</span> changed their cover picture
+              </p>
+          </div>
+            <span>1 min ago</span>
+          </div>
+{/* first*/}
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://www.pexels.com/photo/woman-standing-on-a-rock-under-a-waterfall-15197689/" alt="" />
+              <p>
+                <span>Himasha Nethmini</span> changed their cover picture
+              </p>
+          </div>
+            <span>1 min ago</span>
+          </div>
+{/* second*/}
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://www.pexels.com/photo/woman-standing-on-a-rock-under-a-waterfall-15197689/" alt="" />
+              <p>
+                <span>Himasha Nethmini</span> changed their cover picture
+              </p>
+          </div>
+            <span>1 min ago</span>
+          </div>
+{/* third*/}
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://www.pexels.com/photo/woman-standing-on-a-rock-under-a-waterfall-15197689/" alt="" />
+              <p>
+                <span>Himasha Nethmini</span> changed their cover picture
+              </p>
+          </div>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+{/*other part*/}
+      <div className="item">
+        <span>Online Friends</span>
+        <div className="user">
+          <div className="userInfo">
+            <img
+            src="https://www.pexels.com/photo/man-in-brown-coat-and-gray-backpack-posing-for-a-photo-837358/"
+            alt=""
+            />
+            <div className="online" />
+            <span> Alaric Shiran </span>
+          </div>
+        </div>
+      </div>
+
+      <div className="user">
+          <div className="userInfo">
+            <img
+            src="https://www.pexels.com/photo/person-in-blue-denim-jacket-sitting-on-chair-while-writing-39866/"
+            alt=""
+            />
+            <div className="online" />
+            <span> Kodeesha </span>
+          </div>
+          </div>
+
+          <div className="user">
+          <div className="userInfo">
+            <img
+            src="https://www.pexels.com/photo/man-standing-in-front-of-people-1709003/"
+            alt=""
+            />
+            <div className="online" />
+            <span> Ashen </span>
+          </div>
+          </div>
+
+          <div className="user">
+          <div className="userInfo">
+            <img
+            src="https://www.pexels.com/collections/people-nature-r0zgvaz/"
+            alt=""
+            />
+            <div className="online" />
+            <span> Dinu </span>
+          </div>
+          </div>
+
+          <div className="user">
+          <div className="userInfo">
+            <img
+            src="https://www.pexels.com/photo/aerial-view-of-fisherman-on-boat-240561/"
+            alt=""
+            />
+            <div className="online" />
+            <span> Ariana Grande </span>
+          </div>
+          </div>
+
+          <div className="user">
+          <div className="userInfo">
+            <img
+            src="https://www.pexels.com/photo/silhouette-of-man-standing-on-mountain-cliff-1087735/"
+            alt=""
+            />
+            <div className="online" />
+            <span> Charlie Puth </span>
+          </div>
+          </div>
+
+
+
+
+
+
+
+
+
+
+        </div>
+
+      </div>
+      
+  )
+}
+
+export default rightbar
